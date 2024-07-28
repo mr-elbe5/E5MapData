aae492c974a7177a96d5e1ad7ad980f42729476a
miro <mr@elbe5.de>
2024-07-28 10:03:32 +0200

    base pack, filemanager init to main app

--- Sources/E5MapData/Application/FileManager.swift
+++ Sources/E5MapData/Application/FileManager.swift
@@ -9,19 +9,11 @@ import E5Data
 
 extension FileManager {
     
-    public static var mediaDirURL : URL = FileManager.privateURL.appendingPathComponent("media")
+    public static var mediaDirURL : URL = privateURL.appendingPathComponent("media")
     public static var tilesDirURL : URL = privateURL.appendingPathComponent("tiles")
-    public static var exportGpxDirURL = documentURL.appendingPathComponent("gpx")
-    public static var exportMediaDirURL = documentURL.appendingPathComponent("media")
-    public static var backupDirURL = documentURL.appendingPathComponent("backup")
-    
-    public func initializeAppDirs() {
-        try! FileManager.default.createDirectory(at: FileManager.tilesDirURL, withIntermediateDirectories: true, attributes: nil)
-        try! FileManager.default.createDirectory(at: FileManager.mediaDirURL, withIntermediateDirectories: true, attributes: nil)
-        try! FileManager.default.createDirectory(at: FileManager.exportGpxDirURL, withIntermediateDirectories: true, attributes: nil)
-        try! FileManager.default.createDirectory(at: FileManager.backupDirURL, withIntermediateDirectories: true, attributes: nil)
-        try! FileManager.default.createDirectory(at: FileManager.exportMediaDirURL, withIntermediateDirectories: true, attributes: nil)
-    }
+    public static var exportGpxDirURL = privateURL.appendingPathComponent("gpx")
+    public static var exportMediaDirURL = privateURL.appendingPathComponent("media")
+    public static var backupDirURL = privateURL.appendingPathComponent("backup")
     
     public func logFileInfo(){
         var names = listAllFiles(dirPath: FileManager.default.temporaryDirectory.path)
