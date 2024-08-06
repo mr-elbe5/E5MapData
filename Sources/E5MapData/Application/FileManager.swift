bf22237951cb89282c4194bc075377420b6405e8
miro <mr@elbe5.de>
2024-08-06 15:25:34 +0200

    Update FileManager.swift

--- Sources/E5MapData/Application/FileManager.swift
+++ Sources/E5MapData/Application/FileManager.swift
@@ -14,14 +14,17 @@ extension FileManager {
     public static var tilesDirURL : URL = privateURL.appendingPathComponent("tiles")
     
     public func logFileInfo(){
+        print("temp files:")
         var names = listAllFiles(dirPath: FileManager.default.temporaryDirectory.path)
         for name in names{
             print(name)
         }
+        print("media files:")
         names = listAllFiles(dirPath: FileManager.mediaDirURL.path)
         for name in names{
             print(name)
         }
+        print("preview files:")
         names = listAllFiles(dirPath: FileManager.previewsDirURL.path)
         for name in names{
             print(name)
